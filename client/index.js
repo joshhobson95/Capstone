3cf6cdfd3e5bbaf93c5241d487d4caa2167501c5
Josh Hobson <joshuahobson1995@gmail.com>
2022-09-30 17:33:41 -0600

    ok

--- client/index.js
+++ client/index.js
@@ -54,7 +54,89 @@ const createPokemonCard = (pokemon) => {
     <p id "quantity"> Quantity is ${pokemon.quantity}</p>
     
     
-    <button onclick="updateQuantity(${pokemon.id}, 'quantity'); toggleClass()">Add One Pokemon to Quantity</button>
+    <button onclick="updateQuantity(${pokemon.id}, 'quantity');">Add One Pokemon to Quantity</button>
+    <script>
+    function colorMe() {
+
+      var element = document.getElementById("1");
+      element.classList.toggle("pokemoncontainer");
+      console.log('click');
+    
+    
+    var pictureClick = document.querySelector("#1");
+    
+      pictureClick.addEventListener("click", colorMe)
+    
+    };
+    </script>
+
+  
+
+<select name="games" id="s1">
+
+  <optgroup label="Gen 3">
+    <option value="Ruby">Ruby</option>
+    <option value="Saphire">Saphire</option>
+    <option value="Emerald">Emerald</option>
+    <option value="FireRed">Fire Red</option>
+    <option value="LeafGreen">Leaf Green</option>
+
+  </optgroup>
+
+  <optgroup label="Gen 4">
+    <option value="Diamond">Diamond</option>
+    <option value="Pearl">Pearl</option>
+    <option value="Platinum">Platinum</option>
+    <option value="HeartGold">HeartGold</option>
+    <option value="SoulSilver">SoulSilver</option>
+
+
+  <optgroup label="Gen 5">
+    <option value="Black">Black</option>
+    <option value="White">White</option>
+    <option value="Black2">Black 2</option>
+    <option value="White2">White 2</option>
+
+  <optgroup label="Gen 6">
+    <option value="X">X</option>
+    <option value="Y">Y</option>
+    <option value="Omega Ruby">Omega Ruby</option>
+    <option value="Alpha Saphire">Alpha Saphire</option>
+
+  <optgroup label="Gen 7">
+    <option value="Sun">Sun</option>
+    <option value="Moon">Moon</option>
+    <option value="Ultra Sun">Ultra Sun</option>
+    <option value="Ultra Moon">Ultra Moon</option>
+    <option value="LGEevee">Let's Go Eevee</option>
+    <option value="LGPikachu">Let's Go Pikachu</option>
+
+
+    
+  <optgroup label="Gen 8">
+    <option value="Sword">Sword</option>
+    <option value="Shield">Shield</option>
+    <option value="Brilliant Diamond">Brilliant Diamond</option>
+    <option value="Shining Pearl">Shining Pearl</option>
+    <option value="Legends Arceus">Legends Arceus</option>
+    
+    </optgroup>
+    </select>
+
+    <p id="choosengame">
+        <span class="op"></span>
+    </p>
+    <button onclick="selOpt()">Option Check</button>
+    
+<br><br>
+
+
+
+
+<br> </br>
+
+
+
     
     <br>     
     `
@@ -65,32 +147,27 @@ const createPokemonCard = (pokemon) => {
 form.addEventListener('submit', submitHandler)
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
+function selOpt() {
+    selEl = document.querySelector('#s1');
+    op = selEl.value;
+    document.querySelector('.op').textContent = op;
+ }
 
 
 ////////////////////////////////// submit handler
 function submitHandler(e) {
   e.preventDefault()
 
-
   let name = document.querySelector('#name')
   let picture = document.querySelector('#picture')
   let quantity = document.querySelector('#quantity')
 
-  
-
-
   let bodyObj = {
-
       name: name.value,
       picture: picture.value,
-      quantity: quantity.value
-     
+      quantity: quantity.value   
   }
-
   addPokemon(bodyObj)
-
- 
-//   id.value = ''
   name.value = ''
   picture.value= ''
   quantity.value = ''
@@ -98,6 +175,57 @@ function submitHandler(e) {
 }
 
 //////////////////////////
+
+function colorMe() {
+
+  var element = document.getElementById("1");
+  element.classList.toggle("pokemoncontainer");
+  console.log('click');
+
+
+var pictureClick = document.querySelector("#1");
+
+  pictureClick.addEventListener("click", colorMe)
+
+};
+
+
+// const getAllPokemon2 = () => {
+//   axios.get(`${baseURL}/getPokemon`)
+//       .then((res) => {
+//           console.log(res.data)
+//           displayPokemon2(res.data)
+//       })
+//       .catch((err) => {
+//           console.log(err)
+//       })
+
+
+
+// data.forEach(i =>{
+//   let x = document.getElementById(`${i.id}`);
+//   let y = document.getElementById(`${i.id}`);
+//   let z = document.getElementById(`${i.id}`);
+//   let b = document.getElementById(`${i.id}+btn`)
+//     x.onclick =changeBackground=()=>{
+//       y.classList.toggle("togglebackground");
+//         if(z.style.display==="none"){
+//           z.style.display="block"
+//         } else {
+//           z.style.display="none";
+//         }
+//     }
+//     b.onclick=memorize=()=>{
+//       y.style.display="none";
+//     }
+
+
+
+// })
+
+// }
+
+
 
 // const image = document.querySelector('.grayscale');
 
@@ -110,18 +238,19 @@ function submitHandler(e) {
 
 
 
-// function colorMe(id) {
-
-//   var element = document.getElementById(`#${id}`);
-//   element.classList.toggle("grayscale");
-//   console.log('click');
 
 
-// var pictureClick = document.querySelector(`#${id}`);
 
-//   pictureClick.addEventListener("click", colorMe)
 
-// };
+
+
+
+
+
+
+
+
+
 
 
 
